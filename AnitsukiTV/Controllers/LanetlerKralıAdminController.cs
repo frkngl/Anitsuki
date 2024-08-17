8ade75ec271bcafefc6a109173fdb9ae66365fa3
Furkan Gül <frknglww@gmail.com>
2024-08-17 20:54:05 +0300

    ADMINDE BOLUMLER YERI USER YERI YAPILDI VE SITE ON KISIMDA DEGISIKLIKLER YAPILDI BUGLAR FIXLENDI

--- AnitsukiTV/Controllers/LanetlerKralıAdminController.cs
+++ AnitsukiTV/Controllers/LanetlerKralıAdminController.cs
@@ -8,6 +8,8 @@ using System.Web;
 using System.Web.Helpers;
 using System.Web.Mvc;
 using System.Xml.Linq;
+using PagedList;
+using PagedList.Mvc;
 
 namespace AnitsukiTV.Controllers
 {
@@ -170,14 +172,20 @@ namespace AnitsukiTV.Controllers
 
 
 
-        public ActionResult Anime(string anime)
+        public ActionResult Anime(string anime, int p = 1)
         {
             var degerler = from a in db.TBLANIME select a;
             if (!string.IsNullOrEmpty(anime))
             {
                 degerler = degerler.Where(x => x.ANIMENAME.ToLower().Contains(anime.ToLower()));
             }
-            return View(degerler.ToList());
+            var pageSize = 50;
+            var paginatedResults = degerler.OrderByDescending(x => x.ID).Skip((p - 1) * pageSize).Take(pageSize).ToList();
+            var pagedList = new StaticPagedList<TBLANIME>(paginatedResults, p, pageSize, degerler.Count());
+            ViewBag.PageCount = (int)Math.Ceiling(degerler.Count() / (double)pageSize);
+            ViewBag.CurrentPage = p;
+            ViewBag.Anime = anime; 
+            return View(pagedList);
         }
 
 
@@ -429,6 +437,7 @@ namespace AnitsukiTV.Controllers
         {
             var FindSeason = db.TBLSEASON.Find(id);
             ViewBag.Anime = db.TBLANIME.ToList();
+            ViewBag.SelectedAnimeId = FindSeason.ANIMEID; // Seçili animeyi ekleyin
             return View("UpdateSeason", FindSeason);
         }
 
@@ -468,14 +477,307 @@ namespace AnitsukiTV.Controllers
         }
 
 
+        
+
         public ActionResult Episode(string episode)
         {
             var degerler = from a in db.TBLEPISODE select a;
             if (!string.IsNullOrEmpty(episode))
             {
-                degerler = degerler.Where(x => x.EPISODENAME.ToLower().Contains(episode.ToLower()));
+                degerler = degerler.Where(x => x.TBLANIME.ANIMENAME.ToLower().Contains(episode.ToLower()));
             }
             return View(degerler.ToList());
         }
+
+
+        [HttpGet]
+        public ActionResult AddEpisode()
+        {
+            ViewBag.Anime = db.TBLANIME.ToList();
+            return View();
+        }
+
+
+        [HttpPost]
+        public ActionResult AddEpisode(TBLEPISODE add, HttpPostedFileBase Image, HttpPostedFileBase Video, HttpPostedFileBase Video2, HttpPostedFileBase Video3, HttpPostedFileBase Video4)
+        {
+            if (Image != null && Image.ContentLength > 0)
+            {
+                if (Image.ContentType == "image/jpeg" || Image.ContentType == "image/jpg" || Image.ContentType == "image/png" || Image.ContentType == "image/jfif")
+                {
+                    var file = new FileInfo(Image.FileName);
+                    var fileName = Path.GetFileName(Image.FileName);
+                    fileName = Guid.NewGuid().ToString() + file.Extension;
+                    var path = Path.Combine(Server.MapPath("~/IMG/"), fileName);
+
+                    WebImage rr = new WebImage(Image.InputStream);
+
+                    if (rr.Width > 1000)
+
+                        rr.Resize(800, 800);
+                    rr.Save(path);
+
+                    TBLEPISODE episode = new TBLEPISODE();
+                    episode.ANIMEID = add.ANIMEID;
+                    episode.SEASONID = add.SEASONID;
+                    episode.EPINUMBER = add.EPINUMBER;
+                    episode.STATUS = false;
+                    episode.EPISODENAME = add.EPISODENAME;
+                    episode.BANNER = fileName;
+                    episode.TIME = add.TIME;
+                    episode.DATE = Convert.ToDateTime(DateTime.Now.ToShortDateString());
+
+                    if (Video != null && Video.ContentLength > 0)
+                    {
+                        string yol = Path.Combine("~/Videos/" + Video.FileName);
+                        Video.SaveAs(Server.MapPath(yol));
+                    }
+                    episode.EP1 = Video != null ? Video.FileName.ToString() : null;
+
+                    if (Video2 != null && Video2.ContentLength > 0)
+                    {
+                        string yol2 = Path.Combine("~/Videos/" + Video2.FileName);
+                        Video2.SaveAs(Server.MapPath(yol2));
+                    }
+                    episode.EP2 = Video2 != null ? Video2.FileName.ToString() : null;
+
+                    if (Video3 != null && Video3.ContentLength > 0)
+                    {
+                        string yol3 = Path.Combine("~/Videos/" + Video3.FileName);
+                        Video3.SaveAs(Server.MapPath(yol3));
+                    }
+                    episode.EP3 = Video3 != null ? Video3.FileName.ToString() : null;
+
+                    if (Video4 != null && Video4.ContentLength > 0)
+                    {
+                        string yol4 = Path.Combine("~/Videos/" + Video4.FileName);
+                        Video4.SaveAs(Server.MapPath(yol4));
+                    }
+                    episode.EP4 = Video4 != null ? Video4.FileName.ToString() : null;
+
+                    db.TBLEPISODE.Add(episode);
+                    db.SaveChanges();
+                    TempData["add"] = "success";
+                    return RedirectToAction("Episode");
+                }
+                else
+                {
+                    TempData["error"] = "error";
+                    return View();
+                }
+            }
+            return View();
+        }
+
+
+        public ActionResult UpdateEpisode(int id)
+        {
+            var FindEpisode = db.TBLEPISODE.Find(id);
+            ViewBag.Anime = db.TBLANIME.ToList();
+            ViewBag.SelectedAnimeId = FindEpisode.ANIMEID; // Seçili animeyi ekleyin
+            return View("UpdateEpisode", FindEpisode);
+        }
+
+        [HttpPost]
+        public ActionResult EpisodeSave(TBLEPISODE update, HttpPostedFileBase Image, HttpPostedFileBase Video, HttpPostedFileBase Video2, HttpPostedFileBase Video3, HttpPostedFileBase Video4)
+        {
+            var updateepisode = db.TBLEPISODE.Find(update.ID);
+
+            if (Image != null && Image.ContentLength > 0)
+            {
+                if (Image.ContentType == "image/jpeg" || Image.ContentType == "image/jpg" || Image.ContentType == "image/png" || Image.ContentType == "image/jfif")
+                {
+                    var anipath = Request.MapPath("~/IMG/" + updateepisode.BANNER);
+                    if (System.IO.File.Exists(anipath))
+                    {
+                        System.IO.File.Delete(anipath);
+                    }
+                    var file = new FileInfo(Image.FileName);
+                    var fileName = Path.GetFileName(Image.FileName);
+                    fileName = Guid.NewGuid().ToString() + file.Extension;
+                    var path = Path.Combine(Server.MapPath("~/IMG/"), fileName);
+
+                    try
+                    {
+                        WebImage rr = new WebImage(Image.InputStream);
+                        if (rr.Width > 1000)
+                            rr.Resize(800, 800);
+                        rr.Save(path);
+                        updateepisode.BANNER = fileName;
+                    }
+                    catch (Exception ex)
+                    {
+                        TempData["error"] = "Error uploading image: " + ex.Message;
+                        return View();
+                    }
+                }
+                else
+                {
+                    TempData["error"] = "Invalid image file type";
+                    return View();
+                }
+            }
+
+            if (Video != null && Video.ContentLength > 0)
+            {
+                var videoPath = Request.MapPath("~/Videos/" + Video.FileName);
+                try
+                {
+                    Video.SaveAs(videoPath);
+                    updateepisode.EP1 = Video.FileName;
+                }
+                catch (Exception ex)
+                {
+                    TempData["error"] = "Error uploading video: " + ex.Message;
+                    return View();
+                }
+            }
+
+            if (Video2 != null && Video2.ContentLength > 0)
+            {
+                var videoPath = Request.MapPath("~/Videos/" + Video2.FileName);
+                try
+                {
+                    Video2.SaveAs(videoPath);
+                    updateepisode.EP2 = Video2.FileName;
+                }
+                catch (Exception ex)
+                {
+                    TempData["error"] = "Error uploading video: " + ex.Message;
+                    return View();
+                }
+            }
+
+            if (Video3 != null && Video3.ContentLength > 0)
+            {
+                var videoPath = Request.MapPath("~/Videos/" + Video3.FileName);
+                try
+                {
+                    Video3.SaveAs(videoPath);
+                    updateepisode.EP3 = Video3.FileName;
+                }
+                catch (Exception ex)
+                {
+                    TempData["error"] = "Error uploading video: " + ex.Message;
+                    return View();
+                }
+            }
+
+            if (Video4 != null && Video4.ContentLength > 0)
+            {
+                var videoPath = Request.MapPath("~/Videos/" + Video4.FileName);
+                try
+                {
+                    Video4.SaveAs(videoPath);
+                    updateepisode.EP4 = Video4.FileName;
+                }
+                catch (Exception ex)
+                {
+                    TempData["error"] = "Error uploading video: " + ex.Message;
+                    return View();
+                }
+            }
+
+            updateepisode.ANIMEID = update.ANIMEID;
+            updateepisode.SEASONID = update.SEASONID;
+            updateepisode.EPINUMBER = update.EPINUMBER;
+            updateepisode.EPISODENAME = update.EPISODENAME;
+            updateepisode.TIME = update.TIME;
+
+            db.SaveChanges();
+            TempData["success"] = "Success";
+            return RedirectToAction("Episode");
+        }
+
+
+        public ActionResult ActivePassive4(int id)
+        {
+            var AdminActivePassive = db.TBLEPISODE.Find(id);
+            if (AdminActivePassive.STATUS == true)
+            {
+                AdminActivePassive.STATUS = false;
+            }
+            else
+            {
+                AdminActivePassive.STATUS = true;
+            }
+            db.SaveChanges();
+            return RedirectToAction("Episode");
+        }
+
+        public ActionResult Deleteeee(int id)
+        {
+            var episodefind = db.TBLEPISODE.Find(id);
+            db.TBLEPISODE.Remove(episodefind);
+            db.SaveChanges();
+            TempData["error"] = "Bölüm başarıyla silindi";
+            return RedirectToAction("Episode");
+        }
+
+        public ActionResult EpisodeDetail(int id)
+        {
+            var degerler = db.TBLEPISODE.Where(x=>x.ID == id).ToList();
+            return View(degerler);
+        }
+
+        public ActionResult Users(string user)
+        {
+            var degerler = from a in db.TBLUSER select a;
+            if (!string.IsNullOrEmpty(user))
+            {
+                degerler = degerler.Where(x => x.USERNAME.ToLower().Contains(user.ToLower()));
+            }
+            return View(degerler.ToList());
+        }
+
+
+        public ActionResult UpdateUser(int id)
+        {
+            var FindUser = db.TBLUSER.Find(id);
+            return View("UpdateUser", FindUser);
+        }
+
+        [HttpPost]
+        public ActionResult UserSave(TBLUSER update, HttpPostedFileBase Image)
+        {
+            var updateuser = db.TBLUSER.Find(update.ID);
+            updateuser.MAIL = update.MAIL;
+            updateuser.ABOUT = update.ABOUT;
+            
+            db.SaveChanges();
+            TempData["success"] = "Success";
+            return RedirectToAction("Users");
+        }
+
+        public ActionResult ActivePassive5(int id)
+        {
+            var AdminActivePassive = db.TBLUSER.Find(id);
+            if (AdminActivePassive.STATUS == true)
+            {
+                AdminActivePassive.STATUS = false;
+            }
+            else
+            {
+                AdminActivePassive.STATUS = true;
+            }
+            db.SaveChanges();
+            return RedirectToAction("Users");
+        }
+
+        public ActionResult Deleteeeee(int id)
+        {
+            var userfind = db.TBLUSER.Find(id);
+            db.TBLUSER.Remove(userfind);
+            db.SaveChanges();
+            TempData["error"] = "Kullanıcı başarıyla silindi";
+            return RedirectToAction("Users");
+        }
+
+        public ActionResult UserDetail(int id)
+        {
+            var degerler = db.TBLUSER.Where(x=>x.ID == id).ToList();
+            return View(degerler);
+        }
+
     }
 }
